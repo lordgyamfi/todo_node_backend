7d2c12ec2bb10632f694aee08ed61f303c8393a4
lordgyamfi <lordgyamfi30@gmail.com>
2021-12-22 21:48:31 +0000

    Port change in app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const todoController = require('./controllers/todo_controller.js');
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); 
 
@@ -12,7 +13,7 @@ app.patch('/todo/:todoId', todoController.updateTodoById);
 app.delete('/todo/:todoId', todoController.deleteTodoById);
 app.get('/todo/:todoId', todoController.getTodoById);
 
-app.listen(3000, function(){
+app.listen(PORT, function(){
 console.log('Server has started to run');
 mongoose.connect(process.env.LOCAL_DB) 
 .then(function(){
